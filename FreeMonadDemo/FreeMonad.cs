b47d6af6b1e5e69a0a2419fc9a65fe651a3dbfee
Dmitry Dorogoy <Dmitrii_Dorogoi@epam.com>
2024-06-17 00:00:31 +0200

    FreeMonad added

---
+++ FreeMonadDemo/FreeMonad.cs
@@ -0,0 +1,24 @@
+﻿namespace FreeMonadDemo
+{
+    public class FreeMonad<C>
+    {
+        public C Command { get; private set; }
+        public Func<object, FreeMonad<C>> Next { get; private set; }
+
+        public FreeMonad(C command, Func<object, FreeMonad<C>> next)
+        {
+            Command = command;
+            Next = next;
+        }
+
+        public static FreeMonad<C> Return()
+        {
+            return null;
+        }
+
+        public static FreeMonad<C> WrapAsMonad(C command)
+        {
+            return new FreeMonad<C>(command, _ => Return());
+        }
+    }
+}
