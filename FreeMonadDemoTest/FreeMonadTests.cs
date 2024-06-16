901c4972624d95c911b8cf8432ce761425f51b6d
Dmitry Dorogoy <Dmitrii_Dorogoi@epam.com>
2024-06-17 00:06:40 +0200

    FreeMonadTests added

---
+++ FreeMonadDemoTest/FreeMonadTests.cs
@@ -0,0 +1,23 @@
+﻿using FreeMonadDemo;
+using NUnit.Framework;
+
+namespace FreeMonadDemoTest
+{
+    [TestFixture]
+    public class FreeMonadTests
+    {
+        [Test]
+        public void TestProgramOutputsHelloWorld()
+        {
+            using (var sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+                var program = FreeMonad<ICommand>.WrapAsMonad(new WriteLine("Hello, World!"));
+
+                new ConsoleFreeMonadInterpreter().Interpret(program);
+
+                Assert.AreEqual("Hello, World!\r\n", sw.ToString());
+            }
+        }
+    }
+}
